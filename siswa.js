cdf69533c34b115c98254a46b50dd38d394f5b8e
setiabudi10 <aldevaro51@gmail.com>
2026-07-09 00:15:51 +0700

    Create siswa.js

---
+++ siswa.js
@@ -0,0 +1,73 @@
+const siswa = [
+
+{kode:"6B001", nama:"AIRIN NOVIANI"},
+
+{kode:"6B002", nama:"AISYAH PUTRI"},
+
+{kode:"6B003", nama:"ALFI RIZKI PRATAMA"},
+
+{kode:"6B004", nama:"AYU MULYATI"},
+
+{kode:"6B005", nama:"BILQIS AQILLA USWATUNNISA"},
+
+{kode:"6B006", nama:"DIVA ADITIA PUTRA"},
+
+{kode:"6B007", nama:"DWI SEPTIANI"},
+
+{kode:"6B008", nama:"ELLQIANO ADRIAN FACHRI"},
+
+{kode:"6B009", nama:"ELVITA FIRJAYANTI"},
+
+{kode:"6B010", nama:"FAJRI"},
+
+{kode:"6B011", nama:"FAYSHA DESTIANA AZHAURA"},
+
+{kode:"6B012", nama:"HIKMAH MAULLANA"},
+
+{kode:"6B013", nama:"HILDAN HARDIANSYAH"},
+
+{kode:"6B014", nama:"IKBAL"},
+
+{kode:"6B015", nama:"JZAFRAN ABDUL FATAH"},
+
+{kode:"6B016", nama:"KALILA RIFDA"},
+
+{kode:"6B017", nama:"MIFTAHUL NIZAM"},
+
+{kode:"6B018", nama:"MUHAMAD ALFARIZ"},
+
+{kode:"6B019", nama:"MUHAMAD DIKA"},
+
+{kode:"6B020", nama:"MUHAMAD MAULANA LUTHFIE EL AZZAM"},
+
+{kode:"6B021", nama:"MUHAMAD NUH"},
+
+{kode:"6B022", nama:"MUHAMMAD LUCKY ARJUNA NST"},
+
+{kode:"6B023", nama:"MUHAMMAD RASYIID SAPUTRA"},
+
+{kode:"6B024", nama:"MUKHYATTULLOH"},
+
+{kode:"6B025", nama:"NIKEN APRILIA"},
+
+{kode:"6B026", nama:"NOVALIA"},
+
+{kode:"6B027", nama:"NUR HAYATI"},
+
+{kode:"6B028", nama:"RUBI ANDINI"},
+
+{kode:"6B029", nama:"SAPUTRA"},
+
+{kode:"6B030", nama:"SATRIA MANDAYA"},
+
+{kode:"6B031", nama:"SINTA BELA"},
+
+{kode:"6B032", nama:"TASYA"},
+
+{kode:"6B033", nama:"ULIS UMAIRA"},
+
+{kode:"6B034", nama:"WIWIN WILDA NINGSIH"},
+
+{kode:"6B035", nama:"ZAHWA KHOIRUNISA"}
+
+];
